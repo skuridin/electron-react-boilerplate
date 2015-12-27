5b7b90069982cad95ef4e4dd2f87c38d9bdd7125
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-12-27 19:54:22 +0500

    Extract window config

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const electron = require('electron');
-
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
@@ -9,17 +8,27 @@ let server = cb => cb();
 let mainWindow = null;
 let url = `file://${__dirname}/dist/index.html`;
 
-electron.crashReporter.start();
-
 if (process.env.NODE_ENV === 'development') {
   server = require('./dev-server');
   url = 'http://127.0.0.1:3000';
 }
 
+electron.crashReporter.start();
+
+const WINDOW_CONFIG = {
+  width: 1000,
+  height: 600,
+  minWidth: 1000,
+  minHeight: 600,
+  show: false,
+  x: undefined,
+  y: undefined,
+};
+
 server(() => {
   app.on('window-all-closed', app.quit);
   app.on('ready', () => {
-    mainWindow = new BrowserWindow({ width: 800, height: 600, show: false });
+    mainWindow = new BrowserWindow(WINDOW_CONFIG);
     mainWindow.loadURL(url);
     mainWindow.webContents.on('did-finish-load', () => {
       mainWindow.show();
